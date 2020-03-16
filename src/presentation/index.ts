26c6e85c2039bcad55306fb09c3a4c8c9061ad46
LucasCampioto <l_campioto@hotmail.com>
2020-03-16 15:46:32 -0300

    criando a parte do login

--- src/presentation/index.ts
+++ src/presentation/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
+import { loginEndpoint } from "./loginEndPoint";
 
 const app = express();
 app.use(express.json());
+
+app.post('/login', loginEndpoint );
 
 export default app;
